aa92ec41bc75485757844ab790ec8070ca7ed075
Duncan039 <duncanwalubengo98@gmail.com>
2022-07-28 12:59:12 +0300

    test

---
+++ test.py
@@ -0,0 +1,21 @@
+print("hello world, this is pycharm. I HOPE I NWILL LEARN SOMETHING DIFFICULT AND PROGRESS IN PYTHON THIS TIME ROUND")
+# define main function to print out something
+def main():
+    i = 1
+    max = 10
+    while (i < max):
+        print(i)
+        i=i+1
+# call function main
+main()
+import keyword
+print(keyword.kwlist)
+def main1():
+    name="DUNCAN"
+    for k in name:
+        print(k+" love you")
+main1()
+def dd(num1,num2):
+    N=num1+num2
+    print(N)
+dd(89,1)
